78f3b071a1789e6915bb5d6b5f661b38343447ce
nd-git-hub D <nitinsd600@gmail.com>
2023-09-25 05:02:43 +0000

    #9 updated main.tf and readme for terraform random provider

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-1st change
-2nd test
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 12
+  special          = false
+ }
+
+output "Random_backet_name" {
+  value = random_string.bucket_name.result
+}
