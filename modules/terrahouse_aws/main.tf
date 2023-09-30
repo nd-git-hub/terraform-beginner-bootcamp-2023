10c24fe52f8d4eff696312da184c9ac81ecbe6c1
nd-git-hub <127363867+nd-git-hub@users.noreply.github.com>
2023-09-30 12:19:22 -0400

    #25 aws terrahouse module (#26)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -18,16 +18,21 @@ terraform {
   }
 }
 
-provider "random" {
-  # Configuration options
-}
 
-provider "aws" {
+#provider "aws" {
   # Configuration options
- region = "us-east-1"
- access_key = var.access_key
- secret_key = var.secret_key
- token = var.token
- }
+# region = "us-east-1"
+# access_key = var.access_key
+# secret_key = var.secret_key
+# token = var.token
+# }
 
 #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
+
+resource "aws_s3_bucket" "s3-bucket-tf-bcmp" {
+  bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
