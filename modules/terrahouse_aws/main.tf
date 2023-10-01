531f7b0f06f02fbda443e8700742de63cb9ec2c6
nd-git-hub <127363867+nd-git-hub@users.noreply.github.com>
2023-10-01 18:09:15 -0400

    #31 content version change (#32)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -31,3 +31,4 @@ terraform {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 data "aws_caller_identity" "current" {}
+
--- variable.tf
+++ variable.tf
@@ -25,3 +25,7 @@ variable "index_html_filepath" {
 variable "error_html_filepath" {
   type = string
 }
+
+variable "content_version" {
+  type        = number
+}
