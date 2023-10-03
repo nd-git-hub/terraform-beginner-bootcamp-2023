288ef699d89b2ad7931d88d9e3ff1a11bfbf1930
ndesh git <nitinsd600@gmail.com>
2023-10-03 03:29:15 +0000

    #42 corrected uuid in server.rb

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -77,7 +77,7 @@ class TerraTownsMockServer < Sinatra::Base
   end
 
   def x_user_uuid
-    return 'e328f4ab-b99f-421c-84c9-4ccea042c7d1'
+    return '447ae4bf-7399-4767-ae27-089bb6e8473a'
   end
 
   def find_user_by_bearer_token
