9c31cad07844f942a0023e47dfea4b0e4a87c877
nd-git-hub <127363867+nd-git-hub@users.noreply.github.com>
2023-10-06 21:26:58 -0400

    44 terratowns providers (#46)
    * #44 wip setting up skeleton for creating custom provider
    
    * #47 terraform provider block correction

--- main.tf
+++ main.tf
@@ -1,18 +1,41 @@
+terraform {
+#  cloud {
+#    organization = "ndterra"
+#
+#    workspaces {
+#      name = "terrahouse-cloud"
+#    }
+#  }
+  required_providers {
+#    random = {
+#      source = "hashicorp/random"
+#      version = "3.5.1"
+#    }
+        terratowns = {
+          source = "local.providers/local/terratowns"
+          version = "1.0.0"
+        }
 
-provider "aws" {
-  # Configuration options
- region = "us-east-1"
- access_key = var.access_key
- secret_key = var.secret_key
- token = var.token
- }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+    #     aws = {
+    #   source = "hashicorp/aws"
+    #   version = "5.17.0"
+    # }
+  }
 }
+
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="447ae4bf-7399-4767-ae27-089bb6e8473a" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -1,23 +1,11 @@
 terraform {
-#  cloud {
-#    organization = "ndterra"
-#
-#    workspaces {
-#      name = "terrahouse-cloud"
-#    }
-#  }
   required_providers {
-#    random = {
-#      source = "hashicorp/random"
-#      version = "3.5.1"
-#    }
-        aws = {
+    aws = {
       source = "hashicorp/aws"
-      version = "5.17.0"
+      version = "5.16.2"
     }
   }
 }
-
 
 #provider "aws" {
   # Configuration options
