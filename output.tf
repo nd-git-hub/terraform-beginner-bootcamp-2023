3eb06d19ad99345f50b7777abdea63c7c86ffa08
ndesh git <nitinsd600@gmail.com>
2023-10-07 01:14:57 +0000

    #47 terraform provider block correction

--- output.tf
+++ output.tf
@@ -1,25 +1,25 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_object_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.s3_bucket_object
-}
+# output "s3_object_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.s3_bucket_object
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 Static Website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "s3_OAI" {
-  description = "cloudfrmation OAI name "
-  value = module.terrahouse_aws.origin_access_control_name
-}
+# output "s3_OAI" {
+#   description = "cloudfrmation OAI name "
+#   value = module.terrahouse_aws.origin_access_control_name
+# # }
 
 
-output "cloudfront_url" {
- description = "The CloudFront Distribution Domain Name"
- value = module.terrahouse_aws.website_domain
-}
+# output "cloudfront_url" {
+#  description = "The CloudFront Distribution Domain Name"
+#  value = module.terrahouse_aws.website_domain
+# }
