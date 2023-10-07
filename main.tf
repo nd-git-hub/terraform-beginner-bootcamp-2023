f678ed99f8d9f7c2a7babaa782a70afcc90d6b75
ndesh git <nitinsd600@gmail.com>
2023-10-07 15:49:39 +0000

    completed provider code

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ terraform {
 
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="447ae4bf-7399-4767-ae27-089bb6e8473a" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -39,3 +39,17 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
